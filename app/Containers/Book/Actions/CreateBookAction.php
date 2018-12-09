8b347bddcbf61f3921ecad3a231dd5861c0dac58
wilmer <wilmer.payall@gmail.com>
2018-12-09 19:05:10 -0100

    Call StoreFileTask

--- app/Containers/Book/Actions/CreateBookAction.php
+++ app/Containers/Book/Actions/CreateBookAction.php
@@ -6,12 +6,22 @@ use App\Ship\Parents\Actions\Action;
 use App\Ship\Parents\Requests\Request;
 use Apiato\Core\Foundation\Facades\Apiato;
 
+
 class CreateBookAction extends Action
 {
     public function run(Request $request)
     {
-        $data = $request->sanitizeInput([
-            // add your request data here
+        $folder = 'books';
+        $imageUrl = Apiato::call('Files@StoreFileTask', [$request->file('file'), $folder]);
+
+        $data = $request->merge(['image_url' => $imageUrl])
+          ->sanitizeInput([
+            'name',
+            'description',
+            'copyright',
+            'user_id',
+            'category_id',
+            'image_url'
         ]);
 
         $book = Apiato::call('Book@CreateBookTask', [$data]);
