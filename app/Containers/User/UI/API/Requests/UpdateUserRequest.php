7ff453e045b4f29e1a270b3422431b9db3ef32f5
wilmer <wilmer.payall@gmail.com>
2018-12-18 23:18:12 -0100

    Validate categories_ids

--- app/Containers/User/UI/API/Requests/UpdateUserRequest.php
+++ app/Containers/User/UI/API/Requests/UpdateUserRequest.php
@@ -29,6 +29,7 @@ class UpdateUserRequest extends Request
      */
     protected $decode = [
         'id',
+        'categories_ids.*'
     ];
 
     /**
@@ -51,6 +52,8 @@ class UpdateUserRequest extends Request
             'id'       => 'required|exists:users,id',
             'password' => 'min:6|max:40',
             'name'     => 'min:2|max:50',
+            'categories_ids' => 'required',
+            'categories_ids.*' => 'exists:categories,id',
         ];
     }
 
