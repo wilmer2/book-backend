45f0773be4f4e22fb0dcb88bad77cbe46c5bf3b6
wilmer <wilmer.payall@gmail.com>
2018-12-30 17:24:47 -0100

    Change Class Migration

--- app/Containers/Page/Data/Migrations/2018_12_26_014750_add_like_count_column.php
+++ app/Containers/Page/Data/Migrations/2018_12_26_014750_add_like_count_column.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class AddLikeCountColumn extends Migration
+class AddLikeCountColumnToPage extends Migration
 {
 
     /**
