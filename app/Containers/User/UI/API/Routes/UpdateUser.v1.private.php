56616d2fc43552b14d92e7055b0f4ae626ce859e
Wilmerq <wilmer.payall@gmail.com>
2019-05-30 10:45:51 -0400

    Change method put for patch

--- app/Containers/User/UI/API/Routes/UpdateUser.v1.private.php
+++ app/Containers/User/UI/API/Routes/UpdateUser.v1.private.php
@@ -3,7 +3,7 @@
 /**
  * @apiGroup           Users
  * @apiName            updateUser
- * @api                {put} /v1/users/:id Update User
+ * @api                {patch} /v1/users/:id Update User
  *
  * @apiVersion         1.0.0
  * @apiPermission      Authenticated User
@@ -14,7 +14,7 @@
  * @apiUse             UserSuccessSingleResponse
  */
 
-$router->put('users/{id}', [
+$router->patch('users/{id}', [
     'as' => 'api_user_update_user',
     'uses'       => 'Controller@updateUser',
     'middleware' => [
